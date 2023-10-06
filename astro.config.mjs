51d9f1473321e7a6dc6d3b6eefe962c10f0582e2
Sumit Ghosh <sumit@sumit-ghosh.com>
2023-10-06 14:10:26 +0530

    Overhaul template

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import { autolinkConfig } from "./plugins/rehype-autolink-config";
 import rehypeSlug from "rehype-slug";
 import alpinejs from "@astrojs/alpinejs";
-import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,50 +14,7 @@ export default defineConfig({
 			__DATE__: `'${new Date().toISOString()}'`,
 		},
 	},
-	integrations: [
-		tailwind(),
-		sitemap(),
-		alpinejs(),
-		AstroPWA({
-			mode: "production",
-			base: "/",
-			scope: "/",
-			includeAssets: ["favicon.svg"],
-			registerType: "autoUpdate",
-			manifest: {
-				name: "Astros - Starter Template for Astro with Tailwind CSS",
-				short_name: "Astros",
-				theme_color: "#ffffff",
-				icons: [
-					{
-						src: "pwa-192x192.png",
-						sizes: "192x192",
-						type: "image/png",
-					},
-					{
-						src: "pwa-512x512.png",
-						sizes: "512x512",
-						type: "image/png",
-					},
-					{
-						src: "pwa-512x512.png",
-						sizes: "512x512",
-						type: "image/png",
-						purpose: "any maskable",
-					},
-				],
-			},
-			workbox: {
-				navigateFallback: "/404",
-				globPatterns: ["*.js"],
-			},
-			devOptions: {
-				enabled: true,
-				navigateFallbackAllowlist: [/^\/404$/],
-				suppressWarnings: true,
-			},
-		}),
-	],
+	integrations: [tailwind(), sitemap(), alpinejs()],
 	markdown: {
 		rehypePlugins: [
 			rehypeSlug,
